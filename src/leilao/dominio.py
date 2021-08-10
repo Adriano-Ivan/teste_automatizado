8f2caeabcd0a91cb781a89b849a04d29dfb7af7a
Adriano-Ivan <adrianoivandepaulacampos@gmail.com>
2021-08-10 14:56:10 -0300

    Isolando partes do teste para salientar o aspecto semântico do código.

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -7,8 +7,11 @@ class Usuario:
         self.__nome = nome
         self.__carteira = carteira
 
+    def __lance_e_valido(self, valor):
+        return valor <= self.__carteira
+
     def propoe_lance(self, leilao, valor):
-        if(valor <= self.__carteira):
+        if(self.__lance_e_valido(valor)):
             lance = Lance(self, valor)
             leilao.propoe(lance)
             self.__carteira -= valor
@@ -36,24 +39,35 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0
+        self.menor_lance = 0
+        # self.maior_lance = sys.float_info.min
+        # self.menor_lance = sys.float_info.max
+
+    def __tem_lances(self):
+        return self.__lances
+
+    def __sao_usuarios_diferentes(self, lance):
+        return self.__lances[-1].usuario != lance.usuario
+
+    def __valor_e_maior_que_o_anterior(self, lance):
+        return lance.valor > self.__lances[-1].valor
+
+    def __lance_e_valido(self, lance):
+        return (not self.__tem_lances() or self.__sao_usuarios_diferentes(lance) and
+                self.__valor_e_maior_que_o_anterior(lance))
 
     def propoe(self, lance: Lance):
-        if (not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor):
-            if (lance.valor > self.maior_lance):
-                self.maior_lance = lance.valor
-            if (lance.valor < self.menor_lance):
+        if self.__lance_e_valido(lance):
+            if not self.__tem_lances():
                 self.menor_lance = lance.valor
+            self.maior_lance = lance.valor
 
             self.__lances.append(lance)
-            return True
-
-        if(lance.valor <= self.__lances[-1].valor):
-            raise ValueError('O valor é inválido ! É preciso informar um valor maior que o do lance anterior !')
-
-        if(self.__lances[-1].usuario == lance.usuario):
+        else:
             raise ValueError('O usuário já fez um lance ! Não é permitido executar dois lances seguidos !')
+        # if(lance.valor <= self.__lances[-1].valor):
+        #     raise ValueError('O valor é inválido ! É preciso informar um valor maior que o do lance anterior !')
 
     @property
     def lances(self):
